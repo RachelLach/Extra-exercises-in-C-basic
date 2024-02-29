a06b4b90eab64aa525741521fc58cdd84826058b
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 23:10:13 +0100

    Update: Wrote different conventions with explanations

---
+++ Naming_Conventions/Naming_Conventions/Naming_Conventions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	int file_size; // Snake Case* 
+	int FileSize;  // Pascal Case**
+	int fileSize;  // Camel Case ***
+	int iFileSize; // Hungarian Notation 
+	return 0;
+
+}
+
+// * lower case letters to name variables and constants. 
+// * separating several words with underscore (_).
+
+// ** Capitalize the first letter of every word without underscore between. 
+
+// *** first letter lowercase rest like pascal. 
+
+// **** prefix the name of the variable with a letter that specify -> 
+// their type. here i for integer + pascal case
+
+// camel case for variables and constants 
+// pascal case for classes 
