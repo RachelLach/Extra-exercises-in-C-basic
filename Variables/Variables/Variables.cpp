82cb9935479913d0f32217a7e545868dd042bda7
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 19:46:13 +0100

    Update: Added two variables (int and double ) and print int

--- Variables/Variables/Variables.cpp
+++ Variables/Variables/Variables.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int file_size = 100; // This called initializing 
+    double sales = 9.99; // Initialize it to 9.99
+    std::cout << file_size; // with "text" will print a text 
+    // but now without "" it will print value 100
+    return 0;
 }
