166777296183f95205fecdc5a10ae33200b94bee
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 20:43:19 +0100

    Update: Added some comments that could be a question
    Wrote a code for the question as well

---
+++ Variable-Exercise/Variable-Exercise/Variable-Exercise.cpp
@@ -0,0 +1,27 @@
+// Write a code to swap the value of two variable: 
+
+// 1. Develop a program to exchange the values stored in two variables.
+// 2. Create code that switches the contents of two variables.
+// 3. Craft a script to interchange the values held by two variables.
+// 4. Write a function to swap the values assigned to two variables.
+// 5. Produce code to swap the contents of two variables.
+// 6. Construct a program that performs a swap operation on two variables.
+// 7. Implement a solution to exchange the values between two variables.
+// 8. Devise code to perform a value swap between two variables.
+// 9. Design a function that swaps the values stored in two variables.
+// 10. Generate code to perform a variable value exchange between two variables.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 1;
+	int b = 2;
+	int temp = a; // initialize a third variable 
+	a = b;
+	b = temp; 
+
+	cout << b;
+
+	return 0;
+}
