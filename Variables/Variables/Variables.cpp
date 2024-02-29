c21aa3bf49943c9e7edc8c757f0b6207e2c39341
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 20:01:50 +0100

    Update: Deleted value of int variable and tried to fix it

--- Variables/Variables/Variables.cpp
+++ Variables/Variables/Variables.cpp
@@ -2,9 +2,24 @@
 
 int main()
 {
-    int file_size = 100; // This called initializing 
+    int file_size{}; // This called initializing 
     double sales = 9.99; // Initialize it to 9.99
     std::cout << file_size; // with "text" will print a text 
     // but now without "" it will print value 100
     return 0;
 }
+
+// I tried to delet the value of file_size and then print it.
+// I got an error that i should have initialize it 
+// then I used a suggestion here telling use {}
+// I did and the error is gone but when i run the program i got a result 0
+
+// Here is why: 
+// a feature of C++ called default initialization. When I declare a variable 
+// without explicitly initializing it with a value, its initial value is 
+// indeterminate. In most cases, it will contain garbage or an arbitrary value.
+
+
+// When I print file_size -> 
+// using std::cout, I'm essentially printing whatever value happened 
+// to be in that memory location at the time.
