7ccbb5c10f9397e2a33dd027efdcdebc1194bd96
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 22:46:55 +0100

    Update: wrote more comment for explain const better

--- Constants/Constants/Constants.cpp
+++ Constants/Constants/Constants.cpp
@@ -5,3 +5,5 @@ int main() {
 	return 0;                // and set it to 3.14 
 	                         // we write const before declare the variable 
 }
+
+// so now if I want to change the value of pi later in my code i get an error
