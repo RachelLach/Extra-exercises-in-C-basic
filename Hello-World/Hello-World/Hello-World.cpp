4102cab815a6acd685e96e16fffc1757fe0a1253
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 18:37:59 +0100

    Update: Added Hello World code with a lot of comments
    This is a copy of my code that  I wrote in Notion

---
+++ Hello-World/Hello-World/Hello-World.cpp
@@ -0,0 +1,23 @@
+// for at use C++ standard libraray we should include one of the files in standard->
+// library. iostram is the file which is short for input output string 
+#include <iostream> 
+
+
+// by just type main we are defining a function 
+// then we should type the value that the function will return here is : int 
+// inside the paranteces we write our parameters 
+// inside the braces we can type the code for this function 
+// std -> short for standard library -> this is like a bucket or a container -> 
+// for the features that are currently available to us -> the features that we have 
+// imported on the top 
+// cout -> character out (an object)
+// so using this object we can output one or more characters on the screen 
+
+int main() {
+    std::cout << "Hello World"; // terminate this line by using a semicolon 
+    // -> this hole line on top called statement -> it tells operating system to 
+    // -> what to do 
+    return 0;
+    // zero tells the operating system that our program is gonna terminate correctly.
+    // if we use another value +/- that means our program encountered an error!
+}
