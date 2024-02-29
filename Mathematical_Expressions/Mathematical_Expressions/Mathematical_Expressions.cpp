ceff3cef19fcf48aea1abf9024eabcd57bf73265
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 23:51:01 +0100

    Update: Wrote a simple division code in C++

--- Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
+++ Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
@@ -1,20 +1,26 @@
-// Mathematical_Expressions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	double  x = 10; 
+	int y = 3; 
+	double  z = x / y; // + is addition operator, x+y is operands 
+
+	std::cout << z;
+
+	return 0; 
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// addition, subtraction and multiplication are the same 
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// but for division is another thing: 
+// when both variables are integers then the result is also an int 
+// if I want to change the result to be float then I should change -> 
+// one vatiables type to be a double and division equasion shoud also be double
+// like the code on top otherwise i get int as a result 
+
+// -> % called modulus which returns the remainder of a division 
+// int x = 10; 
+// int y = 3;
+// int z = x % y; -> the result is 1 
+
