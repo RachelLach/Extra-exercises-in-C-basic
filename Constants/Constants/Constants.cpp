85db382992cce54e76b1fd5435b18d4af061404d
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-02-29 22:44:43 +0100

    Update: Made double variable to be constant

---
+++ Constants/Constants/Constants.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	const double pi = 3.14;  // Declare a variable of type double call it pi ->
+	return 0;                // and set it to 3.14 
+	                         // we write const before declare the variable 
+}
