2ba05b2e077f8d5c4a435e7f113cf28ba43347de
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-01 09:54:15 +0100

    Update: More examples about post/pre increment

--- Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
+++ Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
@@ -3,17 +3,21 @@
 int main() {
 	int x = 10; 
 
-	x = x + 1; // Add 1 to x -> x =x + 1; or better way write it like down 
+	// if we apply as postfix: 
+	// 1. the current value af x is going to be assigned to y 
+	// 2. then x will incremented by 1 
+	// so we get x = 11, and y = 10
+	int y = x++; 
 
-	x++; // increment oprator as post fix 
-	x--; // decrement oprator as post fix
-	++x; // as prefix 
-	--x; // as prefix 
+	// if we apply as prefix: 
+	// 1. the value of x will incremented by 1
+	// 2. then the result will stored in z 
+	// so we get x = 12, and z = 11
+	int z = ++x; 
 
-	std::cout << x;
+	std::cout << y;
 
 	return 0; 
 
 }
 
-// Increment and decrement operators 
