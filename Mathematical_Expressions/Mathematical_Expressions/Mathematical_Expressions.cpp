be8e6de868c1518e34af8720e1491aaf73355a0d
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-01 09:26:16 +0100

    Update: Modulus exercise

--- Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
+++ Mathematical_Expressions/Mathematical_Expressions/Mathematical_Expressions.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	double  x = 10; 
+	int x = 10; 
 	int y = 3; 
-	double  z = x / y; // + is addition operator, x+y is operands 
+	int z = x % y; // + is addition operator, x+y is operands 
 
 	std::cout << z;
 
@@ -11,13 +11,7 @@ int main() {
 
 }
 
-// addition, subtraction and multiplication are the same 
 
-// but for division is another thing: 
-// when both variables are integers then the result is also an int 
-// if I want to change the result to be float then I should change -> 
-// one vatiables type to be a double and division equasion shoud also be double
-// like the code on top otherwise i get int as a result 
 
 // -> % called modulus which returns the remainder of a division 
 // int x = 10; 
