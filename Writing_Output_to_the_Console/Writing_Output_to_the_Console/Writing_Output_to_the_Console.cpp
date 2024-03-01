7cd9978b3777e7dbc8f486dac3adef235934bdba
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-01 10:53:17 +0100

    Update: Exercise to get an string in console

--- Writing_Output_to_the_Console/Writing_Output_to_the_Console/Writing_Output_to_the_Console.cpp
+++ Writing_Output_to_the_Console/Writing_Output_to_the_Console/Writing_Output_to_the_Console.cpp
@@ -1,20 +1,28 @@
-// Writing_Output_to_the_Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 10; 
+    // x = 10 in terminal 
+
+
+    std::cout << "x = "; 
+    // std = Standard Output Stream 
+
+    /* After = do not write anything -> 
+    to get the actual value of x */
+    
+    std::cout << x;
+
+    // OBS: need to write both std to get what i want in console
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*In programming : 
+* Stream means sequence of characters 
+* Standard Output is Console or terminal window 
+*/
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//<< called Stream Insertion Operato
+
+// "" is string 
+
