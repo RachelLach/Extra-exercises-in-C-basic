3910f19793fea67f20615ba0fe35e03f96fa1f68
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-04 21:53:34 +0100

    Update: Exercise from F to C

--- Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
+++ Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Enter values for x and y: ";
+    cout << "write a temprature in fahrenheit for me please:  ";
 
     // Read the value (from console/standard input) and put it in a variable ->
     // But first we need to declare a variable 
-    double x; // declare an integer called value - use . instead of ,
-    double y;
-                   
-
-    cin >> x >> y; // >> called stream extraction operator 
+    double fahrenheit; // declare an integer called value - use . instead of ,
+    
+    cin >> fahrenheit; // >> called stream extraction operator 
                   // read it and put it in this variable 
    
-    cout << x + y;// to varify our program works 
+    double celsius = (fahrenheit - 32)/1.8;
+    cout << celsius;// to varify our program works 
     
     return 0;
 
 }
 
+/* exercise: write a program to convert a temperature from fahrenheit to 
+celsius. 
+*/
