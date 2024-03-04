ab84d9280a45ac0f2fc1993a9ab4ccbf6a342865
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-04 21:30:34 +0100

    Update. changed int to double and added comments

--- Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
+++ Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
@@ -7,12 +7,13 @@ int main()
 
     // Read the value (from console/standard input) and put it in a variable ->
     // But first we need to declare a variable 
-    int value; // declare an integer called value 
+    double value; // declare an integer called value 
+                  // use . instead of , 
 
     cin >> value; // >> called stream extraction operator 
-    // read it and put it in this variable 
+                  // read it and put it in this variable 
 
-    cout << value; // to varify our program works 
+    cout << value;// to varify our program works 
     return 0;
 
 }
