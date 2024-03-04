5cde089f0ccac164f7a697aaf81746aa19b43e4a
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-04 21:26:44 +0100

    Update: Added an input and two output code

--- Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
+++ Reading_from_the_Console/Reading_from_the_Console/Reading_from_the_Console.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter a value: ";
+
+    // Read the value (from console/standard input) and put it in a variable ->
+    // But first we need to declare a variable 
+    int value; // declare an integer called value 
+
+    cin >> value; // >> called stream extraction operator 
+    // read it and put it in this variable 
+
+    cout << value; // to varify our program works 
+    return 0;
+
 }
 
