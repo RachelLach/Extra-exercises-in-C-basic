5ea9bdcbd8cdcac618ce60e4c6e9fadccb5bf867
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-05 11:53:28 +0100

    Update: Used a pow function to calculate an area of a circle

--- Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
+++ Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
@@ -4,10 +4,20 @@ using namespace std;
 
 int main(){
 
-    double result = pow(2,3);// 2 in power of three is 8 as result 
-    cout << result;
+    cout << "Enter a radius for a circle: ";
+    double radius;// declare a variable 
+    cin >> radius;// to read the value user enters 
+
+    const double pi = 3.14; 
+    double area = pi * pow(radius, 2);
+    cout << area;
     return 0;
 }
 
+
 // google -> cmath reference 
 
+/* Exercise : write a program to calculate the area of a circle ->
+ask the user to enter the radius of a circle and then it should print the 
+area of a circle. 
+*/
