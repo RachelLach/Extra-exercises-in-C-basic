519768ee1a4318983710bb332924250a5b65ba58
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-05 13:59:56 +0100

    Update: Wrote comments about statically and dynamically typed languages

--- Fundamental_Data_Types/Fundamental_Data_Types/Fundamental_Data_Types.cpp
+++ Fundamental_Data_Types/Fundamental_Data_Types/Fundamental_Data_Types.cpp
@@ -1,20 +1,19 @@
-// Fundamental_Data_Types.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// to declare a variable we need to specify its type 
+// statically-typed languages: C++, C#, Java and TypeScript ->
+// when declare a variable, I need to specify its type and this type cannot ->
+// change throughout the lifetime of my program. 
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// #
+
+// dynamically-type languages: Python, Javascript and Ruby -> 
+// in these languagaes I do not need to give a variable particular type ->
+// the type will be determined based on the value that we assigned to these
+// variables and that type can change throughout the lifetime of my program
