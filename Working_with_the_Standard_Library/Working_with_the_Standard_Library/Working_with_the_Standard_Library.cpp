d7aa64da1e335d9a23774b6229ad2a146c90c482
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-05 11:36:19 +0100

    Update: Deleted code and wrote pow function

--- Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
+++ Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
@@ -4,22 +4,10 @@ using namespace std;
 
 int main(){
 
-    double result = floor(1.2);//input value in () = argument -> 
-                               //double value in () we can store in a variable 
-                               //we are calling floor(1.2), executing this -> 
-                               //we are giving it a value and getting a new value
-
+    double result = pow(2,3);// 2 in power of three is 8 as result 
     cout << result;
     return 0;
 }
 
 // google -> cmath reference 
-
-
-/* line 7: 
-1. Declare a variable of type double
-2. called result 
-3. And set it to the return value of the floor function 
-4. Terminate the statement with a semicolon 
-*/
 
