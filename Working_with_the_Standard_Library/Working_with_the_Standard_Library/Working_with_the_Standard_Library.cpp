d02e9cdc6e86cadab7cfd503861502512812d6b6
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-05 11:32:23 +0100

    Update: Wrote a floor function and got a result

--- Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
+++ Working_with_the_Standard_Library/Working_with_the_Standard_Library/Working_with_the_Standard_Library.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // declare a bunch of useful mathematical functions 
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+
+    double result = floor(1.2);//input value in () = argument -> 
+                               //double value in () we can store in a variable 
+                               //we are calling floor(1.2), executing this -> 
+                               //we are giving it a value and getting a new value
+
+    cout << result;
+    return 0;
 }
+
+// google -> cmath reference 
+
+
+/* line 7: 
+1. Declare a variable of type double
+2. called result 
+3. And set it to the return value of the floor function 
+4. Terminate the statement with a semicolon 
+*/
+
