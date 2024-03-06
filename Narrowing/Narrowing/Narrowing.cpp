ae4014388044d91e73169c148c5a894efae87973
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-06 14:09:11 +0100

    Update: {} in code give the same result here

--- Narrowing/Narrowing/Narrowing.cpp
+++ Narrowing/Narrowing/Narrowing.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main() {
 	int number = 1'000'000; 
 	short another_number = number;
+	// short another_number {number};-> get the same result 
 	cout << another_number;// the result of narrowing conversion here is 16960
 	return 0;
 }
 
 
 /* Initialize a variable of a smaller type using a larger type */
+    
