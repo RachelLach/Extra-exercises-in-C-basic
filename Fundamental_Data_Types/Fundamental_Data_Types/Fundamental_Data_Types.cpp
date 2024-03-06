81fd4a2bc07efc2cac225b02eb11d784d389a4db
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-06 09:34:46 +0100

    Update: Added a link to data types as a comment

--- Fundamental_Data_Types/Fundamental_Data_Types/Fundamental_Data_Types.cpp
+++ Fundamental_Data_Types/Fundamental_Data_Types/Fundamental_Data_Types.cpp
@@ -17,3 +17,8 @@ int main()
 // in these languagaes I do not need to give a variable particular type ->
 // the type will be determined based on the value that we assigned to these
 // variables and that type can change throughout the lifetime of my program
+
+
+// https://www.tutorialspoint.com/cplusplus/cpp_data_types.htm can find a list ->
+// about different data types 
+
