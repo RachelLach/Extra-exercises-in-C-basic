1d9fd32f029a71db9b4e56f3b7a665a08854ede6
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-06 10:30:15 +0100

    Update: Used unsigned keyword and got different results

--- Working_with_Numbers/Working_with_Numbers/Working_with_Numbers.cpp
+++ Working_with_Numbers/Working_with_Numbers/Working_with_Numbers.cpp
@@ -3,18 +3,13 @@
 using namespace std;
 int main()
 {
-    int number = 0xFF; 
+    unsigned int number = -10; 
+    number--; 
     cout << number; 
     return 0; 
 }
 
-
-
-/* represet this number in binary system: int number = 255;
-*  int number = 0b11111111; -> 0 b(binary) binary numbers 11111111
-*  the result is 255 in console */
-
-
-/* represet this number in binary system: int number = 255;
-*  int number = 0xFF; -> 0 x(hexa) hexadecimal numbers FF
-*  the result is 255 in console */
+/* unsigned can not accept negative values 
+*  can give random number as a result 
+*  Here I assign the code to -10 and the result is random and +
+*/
