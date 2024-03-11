181db5a085866247d89abc8b72ca0768276dec45
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 20:34:03 +0100

    Update: Underflowing on int

--- types_size_and_limits/types_size_and_limits/types_size_and_limits.cpp
+++ types_size_and_limits/types_size_and_limits/types_size_and_limits.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main() {
 
-    int number = numeric_limits<int>::max();
-    number++;
+    int number = numeric_limits<int>::min();
+    number--;
     cout << number;
     return 0;
 }
 
 
-/* if a the value I want to store in a type is too large for that type -> 
-   it will wrap around to the types minimum. this is called overflowing  */
+/* if a the value I want to store in a type is too small for that type -> 
+   it will wrap around to the largest value that can store in an int. This is called underflowing */
