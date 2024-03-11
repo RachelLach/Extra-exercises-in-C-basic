fd0df54f0b4e6b71075fe7940e23037c672b101c
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 19:23:34 +0100

    Update: Added set with function from IO map to solution

--- formatting_output/formatting_output/formatting_output.cpp
+++ formatting_output/formatting_output/formatting_output.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Spring" << "Nice" << endl
+    cout << setw(10) << "Spring" << "Nice" << endl
          << "Summer" << "Hot";
     return 0;
 }
 
 /* Stream manipulators in C++ : is a function for manipulating or modifying a stream */
+/* setw() -> short for setwith -> call this function and give it the numbers of characters 
+   for reserving for the data that comes after */
