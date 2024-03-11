804c47f66297f5052a77e4af05e381e141b0b71e
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 19:14:04 +0100

    Update: Added IO map  library to the solution
    IO manipulator

--- formatting_output/formatting_output/formatting_output.cpp
+++ formatting_output/formatting_output/formatting_output.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    
+    cout << "Spring" << "Nice" << endl
+         << "Summer" << "Hot";
+    return 0;
 }
+
+/* Stream manipulators in C++ : is a function for manipulating or modifying a stream */
