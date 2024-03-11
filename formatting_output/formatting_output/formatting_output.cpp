6e94d92dbe5c5bf2ee59cf0ae2584a744f7cdecb
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 19:38:33 +0100

    Update: Used fixed and setprecision() manipulator

--- formatting_output/formatting_output/formatting_output.cpp
+++ formatting_output/formatting_output/formatting_output.cpp
@@ -5,14 +5,9 @@ using namespace std;
 
 int main()
 {
-    cout << left;
-    cout <<  setw(10) << "Spring" << setw(10) << "Nice" << endl
-         <<  setw(10) << "Summer" << setw(10) << "Hot";
+    cout << fixed << setprecision(2) << 12.34567;
     return 0;
 }
 
-/* Stream manipulators in C++ : is a function for manipulating or modifying a stream */
-/* setw() -> short for setwith -> call this function and give it the numbers of characters 
-   for reserving for the data that comes after */ 
-
-/* Left aligned -> sticky manipulator -> once I apply it is going to be in effect until we change it */
+/* fixed manipulator -> printing all part of a float number I write in cout */
+/* setprecision() -> how many digits I want after decimal point */
