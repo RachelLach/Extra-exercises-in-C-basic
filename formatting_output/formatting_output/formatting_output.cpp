d4674b765c5990e1ec91b82088c39a40f2744251
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 19:58:13 +0100

    Update: An exercise for try different manipulators

--- formatting_output/formatting_output/formatting_output.cpp
+++ formatting_output/formatting_output/formatting_output.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 int main()
 {
-    cout << fixed << setprecision(2) << 12.34567;
+    cout << left;
+    cout << fixed;
+    cout << setw(15) << "Course" << setw(10) << "Students" << endl
+         << setw(15) << "C++" << setw(10) << "100" << endl
+         << setw(15) << "JavaScript" << setw(10) << "50" << endl;
+    
     return 0;
 }
 
-/* fixed manipulator -> printing all part of a float number I write in cout */
-/* setprecision() -> how many digits I want after decimal point */
+/* Exercise: Write a program for printing this-> course C++ students 100, course JavaScript student 50*/
+
+/* Solution: << setw(15) << "C++" << setw(10) << right << "100" << left << endl
+             << setw(15) << "JavaScript" << setw(10) << right << "50" << left << endl; */ 
