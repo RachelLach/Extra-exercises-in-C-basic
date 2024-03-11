2ca81985b45d6dad4fc124987ea01a24ee19d896
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-11 19:27:43 +0100

    Update: Added left function from IO map to solution

--- formatting_output/formatting_output/formatting_output.cpp
+++ formatting_output/formatting_output/formatting_output.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main()
 {
-    cout << setw(10) << "Spring" << "Nice" << endl
+    cout << left <<  setw(10) << "Spring" << setw(10) << "Nice" << endl
          << "Summer" << "Hot";
     return 0;
 }
 
 /* Stream manipulators in C++ : is a function for manipulating or modifying a stream */
 /* setw() -> short for setwith -> call this function and give it the numbers of characters 
-   for reserving for the data that comes after */
+   for reserving for the data that comes after */ 
+
+/* Left aligned -> sticky manipulator -> once I apply it is going to be in effect until we change it */
