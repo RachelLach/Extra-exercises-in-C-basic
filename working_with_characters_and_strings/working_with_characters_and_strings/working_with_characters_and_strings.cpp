c40177084b04348c9f15a0a5832f5d869186ab00
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-12 09:22:16 +0100

    Update: Char type and get a numeric presentation of a character

--- working_with_characters_and_strings/working_with_characters_and_strings/working_with_characters_and_strings.cpp
+++ working_with_characters_and_strings/working_with_characters_and_strings/working_with_characters_and_strings.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
+    char ch = 'a'; // declare a char called ch and set to single quotes and between them a
+    cout << +ch; // write + to get the numeric presentation of lowercase a 
     return 0;
 }
 
