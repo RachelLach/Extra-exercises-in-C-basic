b1e7cb1c540cd9c0661523a7174d8d0d6e3bf376
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-12 11:16:15 +0100

    Update: Convert int to double

--- type_conversion/type_conversion/type_conversion.cpp
+++ type_conversion/type_conversion/type_conversion.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
+    int x = 1; 
+    double y = 2.2;
+    double z = x + y; // converting an int to double doesn't result in data loss 
+    cout << z;
+
     return 0;
 }
+
+
+/* Convert data from one to another called casting */
