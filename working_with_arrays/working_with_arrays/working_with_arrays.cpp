f875b94558d3763e086950af84f83f73f57cecd4
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-12 10:32:26 +0100

    Update: An example for array and get a hexa.d number as result

--- working_with_arrays/working_with_arrays/working_with_arrays.cpp
+++ working_with_arrays/working_with_arrays/working_with_arrays.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
+    int numbers[5];  // in [] specify is the number of items(integers) I want to store
+                     // alocate five integers right next to eachother in memory 
+                     // I can access any of these using index 
+    cout << numbers; //------>>>> result is a hexadecimal number 0x7ffeed4ec8f0
     return 0;
 }
+
+/* arrays -> to store an collection of elements like a collection of numbers, dates, strings and ...  */
