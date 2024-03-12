cada78466910ce220534416401841ea346cc8d8c
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-12 10:48:15 +0100

    Update: commented more details about arrays

--- working_with_arrays/working_with_arrays/working_with_arrays.cpp
+++ working_with_arrays/working_with_arrays/working_with_arrays.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    int numbers[5] = {10, 20}; // initializing to a list of values
-    cout << numbers[0];
+    int numbers[] = {10, 20}; // initializing to a list of values
+    cout << numbers[1];
+
     return 0;
 }
 
 
+/* if I have all the initial values, I don't need to provide a size
+   if I don't have all initial values and I want to reserve enogh space for storing all the elements -> 
+   then I have to specify a size here */
