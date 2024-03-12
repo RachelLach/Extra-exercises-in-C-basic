9ef720624e6768349aff523a08804c8928beb94b
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-12 09:26:06 +0100

    Update: Print a name with string type in console

--- working_with_characters_and_strings/working_with_characters_and_strings/working_with_characters_and_strings.cpp
+++ working_with_characters_and_strings/working_with_characters_and_strings/working_with_characters_and_strings.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    char ch = 'a'; // declare a char called ch and set to single quotes and between them a
-    cout << +ch; // write + to get the numeric presentation of lowercase a 
+    string name = "Rachel Lachiani";
+    cout << name;
     return 0;
 }
 
 
 /* char -> for storing a single character */
-/* string -> for storing a sequence of characters */
+/* string -> for storing a sequence of characters */ 
