e7655d638041937be9220c05aeaa4a2382b8f977
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-14 11:17:19 +0200

    Update: More comments for compiler's work automatically

--- Comparison Operators/Comparison Operators/Comparison Operators.cpp
+++ Comparison Operators/Comparison Operators/Comparison Operators.cpp
@@ -7,11 +7,14 @@ int main()
     // we don't know what is stored in x. 
     // So now we want to check to see if x is greater than 5. 
     int x = 10; // declare a variable and set it to 10
-    int y = 5; 
+    double  y = 5; 
     // this piece of code called -> a boolean expression -> produces a boolean value x != 5; 
     //  So we can get the result and store it in a in a boolean variable called result. 
 
-    bool result = x != y; 
+    bool result = x == y; 
+    // In this case the compiler automatically casts or converts x from int to double so it can perform the comparison 
+    // So when compering the values of different types the compiler will automatically cast or convert values from
+    // smaller or less precise types to larger or more precise types. 
 
     cout << boolalpha << result; 
     return 0; 
