653d1622bd2c287807acd8c0c3fc629c4d81e237
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-15 11:54:52 +0200

    Update: Adding third condition

--- Logical Operators/Logical Operators/Logical Operators.cpp
+++ Logical Operators/Logical Operators/Logical Operators.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    int age = 10; 
-    bool is_eligible = age > 18 || age < 65; // snake case -> && logical and opretor to add a second condition 
-                                             // or a second boolean expression 
-    cout << boolalpha << !is_eligible;
+    int age = 20; 
+    int salary = 50'000 ; 
+    bool is_eligible = (age > 18 || age < 65) && (salary > 30'000); 
+    cout << boolalpha << is_eligible;
     return 0;
 }
