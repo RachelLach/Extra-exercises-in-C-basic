0406e7915057167b8e0507ef702398da4acb4bae
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-15 11:49:21 +0200

    Update: Using different operators to see different results

--- Logical Operators/Logical Operators/Logical Operators.cpp
+++ Logical Operators/Logical Operators/Logical Operators.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    int age = 20; 
-    bool is_eligible = age > 18 && age < 65; // snake case -> && logical and opretor to add a second condition 
+    int age = 10; 
+    bool is_eligible = age > 18 || age < 65; // snake case -> && logical and opretor to add a second condition 
                                              // or a second boolean expression 
-    cout << boolalpha << is_eligible;
+    cout << boolalpha << !is_eligible;
     return 0;
 }
