eed99f69885d87232992b387675bb37e8d2b6431
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-16 13:49:04 +0200

    Update: Write an example for one if statement

--- If Statements/If Statements/If Statements.cpp
+++ If Statements/If Statements/If Statements.cpp
@@ -1,20 +1,14 @@
-// If Statements.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int temperature = 30; 
+    if (temperature < 15)   // In () we have a condition or a  boolean expression 
+                            // If the expression is true so we print : 
+        cout << "Cold!";    // This is part of if statement 
+    cout << "Done!";        // This is not part of the if statement and always get executed!
+    return 0; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
