0ddb7abc9e8d27596f179b7b9f3df4a33585e7fa
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-16 13:54:40 +0200

    Update: Wrote an exercise with two print

--- If Statements/If Statements/If Statements.cpp
+++ If Statements/If Statements/If Statements.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main()
 {
-    int temperature = 30; 
-    if (temperature < 15)   // In () we have a condition or a  boolean expression 
-                            // If the expression is true so we print : 
-        cout << "Cold!";    // This is part of if statement 
-    cout << "Done!";        // This is not part of the if statement and always get executed!
+    int temperature = 10; 
+    if (temperature < 15) {  // In () we have a condition or a  boolean expression 
+        // If the expression is true so we print : 
+        cout << "Cold!"; 
+        cout << "Wear warm clothes!";
+    }
     return 0; 
 }
 
-
+// For more than one if statement we should type a pair of braces after the if statement 
