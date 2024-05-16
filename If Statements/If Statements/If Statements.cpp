c071fb8a88b44e8905300140d714e49ee3876304
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-16 14:14:34 +0200

    Update: Extra exercise to use all if statements

--- If Statements/If Statements/If Statements.cpp
+++ If Statements/If Statements/If Statements.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-    int temperature = 100; 
-    if (temperature < 60)   
-        cout << "Cold!";
-    else if (temperature < 90)
-        cout << "Nice!";
+    int Sales = 20'000; 
+    double Commission; 
+    if (Sales <= 10'000)
+        Commission = .1;
+    else if (Sales <= 15'000)
+        Commission = .15;
     else
-        cout << "Hot!"; 
+        Commission = .2; 
+    cout << Commission; 
     
     return 0; 
 }
