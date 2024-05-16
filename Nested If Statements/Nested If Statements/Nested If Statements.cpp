e26545d9bd83118a974292aca50db90d13768356
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-16 14:34:33 +0200

    Update: Wrote a simple example for nested if statement

---
+++ Nested If Statements/Nested If Statements/Nested If Statements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    // US Citizen 
+    // CA Resident: tuition = 0
+    // Non resident: tuition = 1000
+    // Not a US Citizen 
+    // Tuition: 3000
+    bool is_citizen = true;
+    bool ca_resident = true; 
+    short tuition = 0; 
+    // Outer if statement 
+    if (is_citizen) {
+        // Inner if statement 
+        if (ca_resident) tuition = 0;
+        else tuition = 1000;
+    }
+    else tuition = 3000; 
+    return 0; 
+}
