edd4ce248304c7309c1caba7b1c475fb1b66c875
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-17 21:09:47 +0200

    Update: Simplified the code by using conditional operator

--- The Conditional Operator/The Conditional Operator/The Conditional Operator.cpp
+++ The Conditional Operator/The Conditional Operator/The Conditional Operator.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     int sales = 11'000; 
-    double commission; 
-    if (sales > 10'000)
+    double commission = (sales > 10'000) ? .1 : .05; // in () we have a condition if true so set to .1 otherwise .05
+    /*if (sales > 10'000)
         commission = .1;
     else
-        commission = .05; 
+        commission = .05;*/ 
+    cout << commission; 
     return 0; 
 }
 
