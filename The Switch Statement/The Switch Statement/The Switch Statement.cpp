68c172e70b6fdebadd485732449b55e55a36c158
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-20 19:53:15 +0200

    Update: Wrote a basic calculator with switch statement

--- The Switch Statement/The Switch Statement/The Switch Statement.cpp
+++ The Switch Statement/The Switch Statement/The Switch Statement.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
 using namespace std; 
 
-int main()
-{
-    // Printing a menu for the user: 
-    cout << "1- Create account" << endl   // To create an account 
-         << "2- Change password" << endl  // another option like change the password 
-         << "3- Quit" << endl             
-         << "Select an option: ";         // select a menu/an option 
-    
-    short input; 
-    cin >> input; // read the user's selected input 
+int main(){
+	cout << " Give me two numbers: ";
+	int first_number; 
+	int second_number; 
+	cin >> first_number >> second_number;
+	
+	cout << "Give me an operator:";
+	char op; // operator is a reserved keyword so we can not name here operator 
+	cin >> op;
 
-    switch (input) {                              // -> in () is the variable 
-        case 1:                                   // Here add one or more case labels for each value 
-            cout << "You selected 1";             // The logic that shoyld be executed 
-            break;                                // We need to terminate this block using break statement 
-        case 2: 
-            cout << "You selected 2";
-            break;
-        default: 
-            cout << "Goodbye!";
-    } 
-
-
-    if (input == 1)
-        cout << "You selected 1";
-    else if (input == 2)
-        cout << "You selected 2";
-    else
-        cout << "Goodbye!";
-
-    return 0;
+	switch (op) {
+	     case '+':
+			 cout << first_number + second_number;
+			 break;
+		 case '-': 
+			 cout << first_number - second_number;
+			 break;
+	     default:
+			 cout << "Invalid operator!";
+	}
+	
+	return 0; 
 }
 
-// With switch statement can only compare variables with certain values , not for greater or less than. 
-// Only for equality comparison -> use switch statement to compare a variable with set of values. 
+// Exercise: Ctreate a basic calculator. Ask the user for two numbers and an operator. Then, print the result 
+// of applying the operator to those numbers. 
