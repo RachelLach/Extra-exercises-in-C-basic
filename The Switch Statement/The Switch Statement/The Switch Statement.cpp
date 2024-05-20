fd755f5a7e348fdf124d78d31200a8a47fad9485
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-20 19:27:36 +0200

    Update: Wrote switch statement for same exercise

--- The Switch Statement/The Switch Statement/The Switch Statement.cpp
+++ The Switch Statement/The Switch Statement/The Switch Statement.cpp
@@ -12,6 +12,18 @@ int main()
     short input; 
     cin >> input; // read the user's selected input 
 
+    switch (input) {                              // -> in () is the variable 
+        case 1:                                   // Here add one or more case labels for each value 
+            cout << "You selected 1";             // The logic that shoyld be executed 
+            break;                                // We need to terminate this block using break statement 
+        case 2: 
+            cout << "You selected 2";
+            break;
+        default: 
+            cout << "Goodbye!";
+    } 
+
+
     if (input == 1)
         cout << "You selected 1";
     else if (input == 2)
@@ -21,3 +33,6 @@ int main()
 
     return 0;
 }
+
+// With switch statement can only compare variables with certain values , not for greater or less than. 
+// Only for equality comparison -> use switch statement to compare a variable with set of values. 
