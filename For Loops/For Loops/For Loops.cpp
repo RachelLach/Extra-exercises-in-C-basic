2114107fbe43ae9cb994ac3c012ebb5b8475df15
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 10:13:42 +0200

    Printing just odd numbers from 0 to 4

--- For Loops/For Loops/For Loops.cpp
+++ For Loops/For Loops/For Loops.cpp
@@ -5,7 +5,8 @@ int main()
 {
     // print numbers from 1 to 5 
     for (int i = 0;i < 5; i++)
-                             
+       if (i % 2 != 0)                      // instead of printing each number lets say: we only want to print the odd numbers 
+                                // is statement and check the reminder of division of AI by two 
        cout << i << endl;                         
                             
        // in () we have three expressions : 1. declaring our loop or counter variable -> 
@@ -20,4 +21,11 @@ int main()
 
 // For loops for repeating a set of instructions 
 // using loop instead of printing one value of a variable like i, but in each repetition we are going to increment i by 1
-// 
+
+
+
+// How the code on top gets executed: 
+// first the loop variable i or counter variable is initialized to zero
+// then the loop condition is evaluated, so zero is less than five 
+// so the body of the loop (cout) gets executed so thats why we see the number 0 on the terminal
+// at the end of this repetition or iteration, i is incremented by one 
