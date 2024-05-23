3b34bd4fec7291a5de1e322ff90cb4da3b940a8b
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 11:59:59 +0200

    Completed the exercise of getting an average number by using range-base for loop

--- Range-based for loops/Range-based for loops/Range-based for loops.cpp
+++ Range-based for loops/Range-based for loops/Range-based for loops.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main() {
     int temperatures[] = { 30,31,32,33,34,35 }; 
-    int sum = 0; 
-    for (int temperature : temperatures)
-        sum = sum + temperature;           // or sum += temperature;
-
-    double average = sum / sizeof(temperatures) / sizeof(int);
-        cout << temperatures << endl;
+    double sum = 0; 
+    for (int temperature : temperatures)   // range-based for loop we can easily iterate over this array 
+                                           // declare an int called temperature, and in each iteration this temperature 
+                                           // is going to hold one one of the values in this array
+        sum = sum + temperature;           // or sum += temperature; in each iteration we are going to take the sum
+                                           // and add the temperature to it
+    short count = sizeof(temperatures) / sizeof(int);   // devided by the total number of the array 
+                                                        // to get the number of items in this array we need to get
+                                                        // the size of array sizeof(temperatures) / sizeof(int)
+    double average = sum /count  ;
+        cout << average << endl;
     return 0;
 }
 
