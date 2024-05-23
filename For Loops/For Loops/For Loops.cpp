c367ad834bf820bc9aa9d650a75e646b4a72f7a2
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 09:25:14 +0200

    Update: Wrote a simple example without for loop

--- For Loops/For Loops/For Loops.cpp
+++ For Loops/For Loops/For Loops.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main()
 {
+    // print numbers from 1 to 5 
+    cout << 1 << endl; 
+    cout << 2 << endl;
+    cout << 3 << endl;
+    cout << 4 << endl;
+    cout << 5 << endl;
     return 0;
 }
+
+// For loops for repeating a set of instructions 
