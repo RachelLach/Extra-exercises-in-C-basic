fb7461ecf66dccd112302a066010de8221aa5e8e
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 11:35:59 +0200

    Exercise to iterate over a string

--- Range-based for loops/Range-based for loops/Range-based for loops.cpp
+++ Range-based for loops/Range-based for loops/Range-based for loops.cpp
@@ -2,30 +2,19 @@
 using namespace std; 
 
 int main() {
-    int numbers[] = {1,2,3};            // int array called number and initialize this to 1,2 and 3
-    for (int i = 0; i < sizeof(numbers)/sizeof(int); i++) 
-                                          // loop over this array and print each number on a new line 
-    //sizeof (numbers): 16                // problem: we have to calculate it dynamically when we change the size of array 
-    // sizeof(int) : 4                    // solution: function -> sizeof()-> if we pass our array here it will return
-                                          // the number of bytes allocated by this array 
-        cout << numbers[i] << endl;
-
-
-
-    // second implementation 
-    for (int number : numbers)            // in () we declare a loop variable -> this variable is going to hold one 
-        cout << number << endl;           // of the items in this array in each iteration 
-                                          // we are working with actual items or actual integers and not index
+    string name = "Rachel Lachiani";
+    for (char ch : name)  // to iterate over the string -> declare a variable that is gonna hold 
+                          //each carachter in this string in each iteration
+        cout << ch << endl;
     return 0;
 }
 
-// by using the range base for loop we can also iterate over a string, because a string is a sequence 
-// or list of characters. 
 
 
 
 
 
-// now: we have a list of items and we want to iterate or loop over them: that is called the range based for loop 
+
+
 
 
