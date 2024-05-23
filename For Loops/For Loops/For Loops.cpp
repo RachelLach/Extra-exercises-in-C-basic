f16212f649cc804ce63a72e63b1ef20541092ae6
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 10:30:22 +0200

    An exercise for for loop and if statement

--- For Loops/For Loops/For Loops.cpp
+++ For Loops/For Loops/For Loops.cpp
@@ -3,29 +3,19 @@ using namespace std;
 
 int main()
 {
-    // print numbers from 1 to 5 
-    for (int i = 0;i < 5; i++)
-       if (i % 2 != 0)                      // instead of printing each number lets say: we only want to print the odd numbers 
-                                // is statement and check the reminder of division of AI by two 
-       cout << i << endl;                         
-                            
-       // in () we have three expressions : 1. declaring our loop or counter variable -> 
-       // terminate the expression using ; 2. boolean expression or a condition that determines ->
-       // when this loop will terminate-> then we terminate this by using a ; , 
-       // 3. type an expression that determines how is going to change after each repetition 
-       // i = i + 1 or i++
-       // Then we write the statement that we want to repeat, for multiple statements use {}                        
-                       
+    cout << "Enter a positive number: "; 
+    int number; 
+    cin >> number; 
+    
+    if (number < 0)
+        cout << "Error! Number is not positive.";
+    else {
+        int factorial = 1; 
+        for (int i = 1; i <= number; i++)
+            factorial = factorial * i;         // or factorial *= i
+        cout << "The factorial of " << number << ": " << factorial;
+    }         
     return 0;
-}
+} 
 
-// For loops for repeating a set of instructions 
-// using loop instead of printing one value of a variable like i, but in each repetition we are going to increment i by 1
-
-
-
-// How the code on top gets executed: 
-// first the loop variable i or counter variable is initialized to zero
-// then the loop condition is evaluated, so zero is less than five 
-// so the body of the loop (cout) gets executed so thats why we see the number 0 on the terminal
-// at the end of this repetition or iteration, i is incremented by one 
+// Exercise: Ask the user for a positive number. Print the factorial of that number. 
