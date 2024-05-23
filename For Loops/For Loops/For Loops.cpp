1acd3a4ae3e0c72df77e3f0be10b6ee46a656ec8
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 10:09:14 +0200

    Wrote a for loop for the same exercise with comments

--- For Loops/For Loops/For Loops.cpp
+++ For Loops/For Loops/For Loops.cpp
@@ -4,12 +4,20 @@ using namespace std;
 int main()
 {
     // print numbers from 1 to 5 
-    cout << 1 << endl; 
-    cout << 2 << endl;
-    cout << 3 << endl;
-    cout << 4 << endl;
-    cout << 5 << endl;
+    for (int i = 0;i < 5; i++)
+                             
+       cout << i << endl;                         
+                            
+       // in () we have three expressions : 1. declaring our loop or counter variable -> 
+       // terminate the expression using ; 2. boolean expression or a condition that determines ->
+       // when this loop will terminate-> then we terminate this by using a ; , 
+       // 3. type an expression that determines how is going to change after each repetition 
+       // i = i + 1 or i++
+       // Then we write the statement that we want to repeat, for multiple statements use {}                        
+                       
     return 0;
 }
 
 // For loops for repeating a set of instructions 
+// using loop instead of printing one value of a variable like i, but in each repetition we are going to increment i by 1
+// 
