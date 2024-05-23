5435e60124bb860942d37630f57af16220c47bbb
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 11:02:18 +0200

    Wrote a simple array and print number from it

--- Range-based for loops/Range-based for loops/Range-based for loops.cpp
+++ Range-based for loops/Range-based for loops/Range-based for loops.cpp
@@ -1,20 +1,12 @@
-// Range-based for loops.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int numbers[] = { 1,2,3 };      // int array called number and initialize this to 1,2 and 3
+    for (int i = 0; i < 3; i++)     // loop over this array and print each number on a new line 
+        cout << numbers[i] << endl;
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Looping over a range or a list of values 
