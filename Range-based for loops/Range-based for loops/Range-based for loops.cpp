21d9f9dc8e58c5f318b2590e20f8ca03d9460d1e
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-23 11:48:46 +0200

    Write an example using array and range base for loop

--- Range-based for loops/Range-based for loops/Range-based for loops.cpp
+++ Range-based for loops/Range-based for loops/Range-based for loops.cpp
@@ -2,15 +2,18 @@
 using namespace std; 
 
 int main() {
-    string name = "Rachel Lachiani";
-    for (char ch : name)  // to iterate over the string -> declare a variable that is gonna hold 
-                          //each carachter in this string in each iteration
-        cout << ch << endl;
+    int temperatures[] = { 30,31,32,33,34,35 }; 
+    int sum = 0; 
+    for (int temperature : temperatures)
+        sum = sum + temperature;           // or sum += temperature;
+
+    double average = sum / sizeof(temperatures) / sizeof(int);
+        cout << temperatures << endl;
     return 0;
 }
 
 
-
+// Exercise: Given a list of temperatures, write code to calculate the average temperature. 
 
 
 
