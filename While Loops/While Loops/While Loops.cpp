e52a2538c0d1334f34c5549773abf56d0aba9c6b
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-24 13:48:24 +0200

    An exercise for a user to guess a secret number

--- While Loops/While Loops/While Loops.cpp
+++ While Loops/While Loops/While Loops.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main()
 {
-    int number = 0; 
-    while (number < 1 || number > 5)         // Continuously ask a user to enter a number between one to five 
-        cout << "Number: " << endl; 
-        cin >> number; 
-        if (number < 1 || number > 5)
-            cout << "Enter a number between 1 and 5!" << endl;
-        
+	int secret_number = 5; 
+	int input = 0;  
+	while (input != secret_number)
+		cout << " Guess a number between 1 and 5 : " << endl; 
+	    cin >> input; 
+   
+	return 0;
 }
+
+// Exercise:Declare a secret number. Continuously ask the user to guess the secret number :)
