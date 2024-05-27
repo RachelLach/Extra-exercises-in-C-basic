214c2286664c6ae5275f995b4028fc3fcdd9aba5
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-27 15:40:32 +0200

    Using a small function to solve a complex problem!

--- Defining and Calling Functions/Defining and Calling Functions/Defining and Calling Functions.cpp
+++ Defining and Calling Functions/Defining and Calling Functions/Defining and Calling Functions.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std; 
 
-// How about a function that returns a value?
+
+void greet(string name) {
+    cout << "Hello " << name << endl;
+}
+
+// Now I reuse the full_name function to create a full name for a person and then I pass the full name to greet function 
 string full_name(string first_name,string last_name) {
     // Concatination (combining)
     return first_name + " " + last_name;
@@ -9,9 +14,9 @@ string full_name(string first_name,string last_name) {
  
 
 int main() { 
-   // store it in a string which we can call name
-   string name= full_name("Rachel", "Lachiani");
-   cout << name << endl;
+   
+   string name = full_name("Rachel", "Lachiani");
+   greet(name);
     
 }
 
