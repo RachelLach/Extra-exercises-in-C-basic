f4934ec74b019cd1d01281b95c30567fa245cf74
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-27 17:14:59 +0200

    A simple example for a function with parameters with default value

--- Parameters with a Default value/Parameters with a Default value/Parameters with a Default value.cpp
+++ Parameters with a Default value/Parameters with a Default value/Parameters with a Default value.cpp
@@ -1,20 +1,19 @@
-// Parameters with a Default value.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
+
+// A function for calculating the tax
+double calculate_tax(double income, double tax_rate =.2) {
+    return income * tax_rate; 
+}
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double tax = calculate_tax(10'000, 0.3);
+    cout << tax << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
