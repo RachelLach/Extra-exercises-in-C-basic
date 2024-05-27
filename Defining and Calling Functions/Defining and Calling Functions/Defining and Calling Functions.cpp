215e4426c01c51b7cf4b9745ede6f97bed5feb4c
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-27 14:57:31 +0200

    Calling the function in the main function

--- Defining and Calling Functions/Defining and Calling Functions/Defining and Calling Functions.cpp
+++ Defining and Calling Functions/Defining and Calling Functions/Defining and Calling Functions.cpp
@@ -3,25 +3,23 @@ using namespace std;
 
 // An example: 
 void greet() {
-    cout << "Hello World!";
+    cout << "Hello World!" << endl;
 }
 
+ 
 
-int main()          // The type of return value of function -> here is an int 
-                    // But if a function doesn't return a value? -> then I need to use a void keyword 
-{                   // Then I type the name of the function -> here is main (should be proper and meningful)
-                    // Then I type () and inside of () I type 0 or more parameters for this function 
-                    // Then {} to define a block of code -> called the body of this function 
-                    // So with this way I defined a function that I can reuse in the program multiple times 
-                    // like the remore control :) 
+int main() { 
+
+    // So now to use the greet function in main function I type the name of the function followed by ().
+    // Calling - Invoking - executing this function 
+    greet(); 
+
+    // Anything that I type after calling the function gets executed 
+    cout << "Done!" << endl; 
     return 0; 
 }
 
 
-
-// Several functions -> each responsible for a single task 
-// Until now I hade just a single function -> main function 
- 
 
 
 
