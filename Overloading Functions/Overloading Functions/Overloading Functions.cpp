6347556c9c1a2e96d081872a59d3c369838430f4
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-28 10:15:43 +0200

    Added more comments to explain more about overloading functions

--- Overloading Functions/Overloading Functions/Overloading Functions.cpp
+++ Overloading Functions/Overloading Functions/Overloading Functions.cpp
@@ -5,16 +5,15 @@ void greet(string name) {
     cout << "Hello " << name; 
 }
 
-// This second version can have any type of parameters 
+// This second version can have any number and type of parameters 
 void greet(string title, string name) {  // These parameters are completely independent of each other 
     cout << "Hello " << title << " " << name; 
 
 }
 
 
-
 int main(){
-    greet("Ms", "Rachel");
+    greet("Ms", "Rachel"); // Two coices here -> then call it and pass it 
     return 0;
 }
 
