c0a493ad3adaafdf62cc49503325447a62c244e3
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-28 11:40:53 +0200

    An example for a side effect of reference a parameter and using const

--- Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
+++ Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std; 
 
-// Another solution: instead of copying them between function calls, I use a single instance of a variable in memory
-// By adding & after the type of parameter -> this called a refrence parameter -> refrences an existing variable 
-// So when i call this function, this variable that I have in main is essentially passed to the function on top.
-// double price = 100; no copy is taken and that means any changes I make to this price in increase_price function 
-// is gonna be visible outside of this function-> when updating the price the changes are impacting this price variable
-// in main function -> this means i don't need to return the updated price 
-// 
-void increase_price(double& price) {
-    price *= 1.2;      
+// I do not need to use passing an argument by reference for reference when it comes to numbers and integers, because
+// double only takes eight bytes of memory, copying eight byte is faster than nothing to worry about!But for arguments
+// with large amount of data -> for improving the efficiency of the program by passing arguments by reference -> like
+// dealing with strings 
+
+void greet(const string& name) {
+    cout << "Hello " << name << endl;
 }
 
 
 int main()
 {
-    double price = 100;
-    increase_price(price);
-    cout << price;
+    string name = "Rachel";
+    greet(name);  
     return 0; 
 }
 
+// After runing the program I get hello Rachel and a-> accidentlly modified the code -> side effect of using reference a parameter
+// for preventing this I can set the (string& name) to const 
 
