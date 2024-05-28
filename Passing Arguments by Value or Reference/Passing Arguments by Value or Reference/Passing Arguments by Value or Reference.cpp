7148065dc4045a3245a12190f0ef9da70812fdc0
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-28 11:04:16 +0200

    Wrote a solution for the wrong value I got earlier

--- Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
+++ Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 using namespace std; 
 
-void increase_price(double price) {
-    // double price; -> explanation in the comments
-    price *= 1.2;             // Increasing the price by 20% ->  price = price * 1.2;
+// Solution: Return the updated price from this function: 
+// 1. change the return type to double,
+// then I return the updated price 
+double increase_price(double price) {
+    price *= 1.2;     
+    return price; 
 }
 
 
-
+// slolution continue here: When I call the increase_price, I get the result and store it in the price variable.
 int main()
 {
     double price = 100;
-    increase_price(price);
+    price = increase_price(price);
     cout << price;
     return 0; 
 }
 
-// When I run this code I get 100 and not the updated value: Here is why: 
-// When I call this function in main and pass the price in calling function, a copy of the value stored in this variable 
-// is taken (double price = 100;) and stored in this parameter (double price) on top. -> the parameters of a function 
-// are like local variables on that function. So this parameters is like having a variable called price in this function. 
-// double price; Also When I declare a variable, that variable is only accesseble with in the block in which is declared. 
-// So the price variable in increase_price function is completely independent of the other price variable in main function.
-// double price = 100;-> this variable is only accessible in main block. -> this is called the scope of a variable 
-// So I have two variables with same name but they have different scopes, They are essentially in two different locations 
-// in memory -> so when calling the increase_price function in main, a copy of the value stored in this variable is taken,
-// and stored in this other location in memory (double price) on top. 
