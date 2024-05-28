195541b2528edb95ca78905327383ed83bc654df
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-28 11:20:01 +0200

    Another solution by using passing an argument with reference

--- Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
+++ Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std; 
 
-double increase_price(double price) {
-    price *= 1.2;     
-    return price; 
+// Another solution: instead of copying them between function calls, I use a single instance of a variable in memory
+// By adding & after the type of parameter -> this called a refrence parameter -> refrences an existing variable 
+// So when i call this function, this variable that I have in main is essentially passed to the function on top.
+// double price = 100; no copy is taken and that means any changes I make to this price in increase_price function 
+// is gonna be visible outside of this function-> when updating the price the changes are impacting this price variable
+// in main function -> this means i don't need to return the updated price 
+// 
+void increase_price(double& price) {
+    price *= 1.2;      
 }
 
 
 int main()
 {
     double price = 100;
-    price = increase_price(price);
+    increase_price(price);
     cout << price;
     return 0; 
 }
 
-// So by default: arguments are passed by value in C++.
-// But what about arguments that can contain large amount of data, this copy operation can be expensive -> no worries: 
-// Then I can pass arguments by reference 
+
