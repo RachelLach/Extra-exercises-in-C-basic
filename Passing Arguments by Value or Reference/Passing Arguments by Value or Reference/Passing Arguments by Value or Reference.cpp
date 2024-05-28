1947c221afef3faf39b96342ed14966aa37d8775
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-28 11:07:57 +0200

    Needed more explanation in form of comments

--- Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
+++ Passing Arguments by Value or Reference/Passing Arguments by Value or Reference/Passing Arguments by Value or Reference.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std; 
 
-// Solution: Return the updated price from this function: 
-// 1. change the return type to double,
-// then I return the updated price 
 double increase_price(double price) {
     price *= 1.2;     
     return price; 
 }
 
 
-// slolution continue here: When I call the increase_price, I get the result and store it in the price variable.
 int main()
 {
     double price = 100;
@@ -19,3 +15,6 @@ int main()
     return 0; 
 }
 
+// So by default: arguments are passed by value in C++.
+// But what about arguments that can contain large amount of data, this copy operation can be expensive -> no worries: 
+// Then I can pass arguments by reference 
