169f9eda28ca22f167ceca62e28f8a7f6895f66e
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-29 11:29:55 +0200

    Declaring a function before main and explain it  with comments

--- Declaring Functions/Declaring Functions/Declaring Functions.cpp
+++ Declaring Functions/Declaring Functions/Declaring Functions.cpp
@@ -1,20 +1,23 @@
-// Declaring Functions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std; 
+
+void greet(string name); 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    greet("Rachel");  // I get an error here for calling the function! cause the function is not visible to the main 
+                      // Thats why I should always define functions before main function 
+                      // But if I really need to declare a function after main then i need to call the function before
+                      // main function -> like in line 4
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void greet(string name) {
+    cout << "Hello " << name; 
+}
+
+
+
+
+// Define a function after main function:
