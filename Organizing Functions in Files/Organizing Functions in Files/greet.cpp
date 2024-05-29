3e0d1bdf46fe410b84d273d7b0192bf24988c17f
Rachel Lachiani <rachel.lachiani@student.forsbergsskola.se>
2024-05-29 15:00:33 +0200

    Deleted the comments

--- Organizing Functions in Files/Organizing Functions in Files/greet.cpp
+++ Organizing Functions in Files/Organizing Functions in Files/greet.cpp
@@ -2,11 +2,6 @@
 #include <iostream>
 using namespace std;
 
-// Now I have two complition errors : because string and cout are not defined. 
-// So I need to include <iostream>
-// Also adding namespace std like in main 
-// now all errors are gone 
-// I do not need to include the header file that contains the declaration for this function 
 
 void greet(string name) {
 	cout << "Hello " << name;
